594fa18854453549d172e8376da9d8c10f48b548
dim test <dimkor20004@gmail.com>
2026-07-25 20:09:06 +0300

    Add flexible station count

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -3,23 +3,38 @@ package ru.netology;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int stationCount;
+
+    // Конструктор без параметров (по умолчанию 10 станций)
+    public Radio() {
+        this.stationCount = 10;
+    }
+
+    // Конструктор с параметром (задаём количество станций)
+    public Radio(int stationCount) {
+        this.stationCount = stationCount;
+    }
 
     public int getCurrentStation() {
         return currentStation;
+    }
+
+    public int getStationCount() {
+        return stationCount;
     }
 
     public void setCurrentStation(int newStation) {
         if (newStation < 0) {
             return;
         }
-        if (newStation > 9) {
+        if (newStation >= stationCount) {
             return;
         }
         currentStation = newStation;
     }
 
     public void next() {
-        if (currentStation < 9) {
+        if (currentStation < stationCount - 1) {
             currentStation = currentStation + 1;
         } else {
             currentStation = 0;
@@ -30,7 +45,7 @@ public class Radio {
         if (currentStation > 0) {
             currentStation = currentStation - 1;
         } else {
-            currentStation = 9;
+            currentStation = stationCount - 1;
         }
     }
 
